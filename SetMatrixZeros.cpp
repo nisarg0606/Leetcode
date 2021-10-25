43d12252be0afd01e0e588993f8e718ca48eea30
nisarg0606 <nisarg0606@gmail.com>
2021-10-25 19:26:54 +0530

    SetMatrixZeros

---
+++ SetMatrixZeros.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool first_row = false, first_col = false;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    if (i == 0)
+                        first_row = true;
+                    if (j == 0)
+                        first_col = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (first_row)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                matrix[0][i] = 0;
+            }
+        }
+        if (first_col)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
